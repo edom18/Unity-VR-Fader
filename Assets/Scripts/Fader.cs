0a1217613c5900845cfbc8a3bd62a8856e562e54
edom18 <edo.m18@gmail.com>
2015-12-12 20:51:46 +0900

    First commit.

---
+++ Assets/Scripts/Fader.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fader : MonoBehaviour {
+
+	public GameObject FaderObject;
+
+	private bool isFaded = false;
+	private Fade fade;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isFaded) {
+			return;
+		}
+
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			isFaded = true;
+
+			GameObject faderIns = Instantiate(FaderObject, Camera.main.transform.position, Quaternion.identity) as GameObject;
+			fade = faderIns.GetComponent<Fade>();
+			fade.OnFadeEnd += fadeEndHandler;
+			fade.FadeOut();
+		}
+	}
+
+	void fadeEndHandler() {
+		fade.OnFadeEnd -= fadeEndHandler;
+		fade.FadeIn();
+	}
+}
