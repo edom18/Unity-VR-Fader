8742f94dd26c0e1c0c5e98b9863d5fb442e071e7
edom18 <edo.m18@gmail.com>
2015-12-22 23:38:05 +0900

    サンプル用スクリプトを追加

---
+++ Assets/VRFader/Scripts/Sample.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sample : MonoBehaviour {
+
+	private bool m_Toggle = false;
+
+	private Fader m_Fader;
+
+	// Use this for initialization
+	void Start () {
+		m_Fader = GetComponent<Fader>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space)) {
+			if (m_Fader.IsFading) {
+				return;
+			}
+
+			m_Toggle = !m_Toggle;
+			if (m_Toggle) {
+				m_Fader.FadeOut();
+			}
+			else {
+				m_Fader.FadeIn();
+			}
+		}
+	}
+}
